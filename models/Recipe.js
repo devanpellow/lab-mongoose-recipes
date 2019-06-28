5c03914f00557575155f5674c3386176c1a83960
Devan Pellow <devanpellow@gmail.com>
2019-06-28 12:02:37 +0200

    iteration two

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-  title: {
-    title: String,
-    required: true,
-    unique: true
-  },
-  level: {
-    type: String,
-    enum: ["Easy Peasy", 'Amateur Chef', 'UltraPro Chef']
-  },
-  ingredients: [String],
-  cuisine: String,
-  dishType: {
-    type: String,
-    enum: ['Breakfast', 'Dish', 'Snack', 'Desseret', 'Other']
-  },
-  image: {
-    type: String,
-    default: "https://images.media-allrecipes.com/images/75131.jpg"
-  },
-  duration: {
-    type: Number,
-    min: 0
-  },
-  creator: String,
-  created: {
-    type: Date,
-    default: Date.now
-  }
+	// TODO: write the schema
+	title: {
+		title: String,
+		// required: true,
+		// unique: true
+	},
+	level: {
+		type: String,
+		enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+	},
+	ingredients: [String],
+	cuisine: String,
+	dishType: {
+		type: String,
+		enum: ["Breakfast", "Dish", "Snack", "Desseret", "Other"]
+	},
+	image: {
+		type: String,
+		default: "https://images.media-allrecipes.com/images/75131.jpg"
+	},
+	duration: {
+		type: Number,
+		min: 0
+	},
+	creator: String,
+	created: {
+		type: Date,
+		default: new Date()
+	}
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;
-
