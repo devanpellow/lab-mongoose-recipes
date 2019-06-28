159a551a2783847a765ffe8f8b754dfff858b7b0
Devan Pellow <devanpellow@gmail.com>
2019-06-28 11:22:55 +0200

    iteration one

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
-
+mongoose
+	.connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
+	.then(() => {
+		console.log("Connected to Mongo!");
+	})
+	.catch(err => {
+		console.error("Error connecting to mongo", err);
+	});
