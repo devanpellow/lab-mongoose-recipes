468eabce2de3979a1cdb4a7d8775faa19506d7e9
Devan Pellow <devanpellow@gmail.com>
2019-06-28 13:00:10 +0200

    add Poutine

--- index.js
+++ index.js
@@ -18,23 +18,13 @@ mongoose
 	});
 
 Recipe.create({
-	title: "Asian Glazed Chicken Thighs",
-	level: "Amateur Chef",
-	ingredients: [
-		"1/2 cup rice vinegar",
-		"5 tablespoons honey",
-		"1/3 cup soy sauce (such as Silver Swan®)",
-		"1/4 cup Asian (toasted) sesame oil",
-		"3 tablespoons Asian chili garlic sauce",
-		"3 tablespoons minced garlic",
-		"salt to taste",
-		"8 skinless, boneless chicken thighs"
-	],
-	cuisine: "Asian",
+	title: "Poutine",
+	level: "UltraPro Chef",
+	ingredients: ["Pommes", "Cheese Curds", "Gravy"],
+	cuisine: "French Canadian",
 	dishType: "Dish",
-	image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
 	duration: 40,
-	creator: "Chef LePapu"
+	creator: "Fernand Lachance"
 });
 
 // console.log(Recipe.title);
